b2429288232bdd8fedd44377205cbc8451027c45
gnabaev <gnabaev@gmail.com>
2024-05-18 21:23:18 +0300

    Создан компонент поисковой строки.

--- dist/app.js
+++ dist/app.js
@@ -1124,7 +1124,6 @@
         }
 
         render() {
-            this.el.innerHTML = '';
             this.el.classList.add('header');
             this.el.innerHTML = `
             <div>
@@ -1148,6 +1147,38 @@
         }
     }
 
+    class Search extends DivComponent {
+        constructor(state) {
+            super();
+            this.state = state;
+        }
+
+        search() {
+            const value = this.el.querySelector('input').value;
+            this.state.searchQuery = value;
+        }
+
+        render() {
+            this.el.classList.add('search');
+            this.el.innerHTML = `
+            <div class="search__wrapper">
+                <input class="search__input" type="text" placeholder="Найти книгу или автора..." value="${this.state.searchQuery ? this.state.searchQuery : ''}"/>
+                <img src="/static/search.svg" alt="Иконка поиска" />
+            </div>
+            <button aria-label="Искать">
+                <img src="/static/search-white.svg" alt="Иконка поиска" />
+            </button>
+        `;
+            this.el.querySelector('button').addEventListener('click', this.search.bind(this));
+            this.el.querySelector('input').addEventListener('keydown', (event) => {
+                if (event.code === 'Enter') {
+                    this.search();
+                }
+            });
+            return this.el;
+        }
+    }
+
     class MainView extends AbstractView {
         state = {
             list: [],
@@ -1160,6 +1191,7 @@
             super();
             this.appState = appState;
             this.appState = onChange(this.appState, this.appStateHook.bind(this));
+            this.state = onChange(this.state, this.stateHook.bind(this));
             this.setTitle('Поиск книг');
         }
 
@@ -1169,8 +1201,23 @@
             }
         }
 
+        async stateHook(path) {
+            if (path === 'searchQuery') {
+                this.state.loading = true;
+                const data = await this.loadList(this.state.searchQuery, this.state.offset);
+                this.state.loading = false;
+                this.state.list = data.docs;
+            }
+        }
+
+        async loadList(q, offset) {
+            const res = await fetch(`https://openlibrary.org/search.json?q=${q}&offset=${offset}`);
+            return res.json();
+        }
+
         render() {
             const main = document.createElement('div');
+            main.append(new Search(this.state).render());
             this.app.innerHTML = '';
             this.app.append(main);
             this.renderHeader();
